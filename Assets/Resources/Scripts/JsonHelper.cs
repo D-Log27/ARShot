31bc6b71fce7804970ffe4e3dd3ae349e30e0b01
Seungsu Baek <tmdtn1608@gmail.com>
2022-03-21 16:29:23 +0900

    글로벌 설정 json화 기능 추가

--- Assets/Resources/Scripts/JsonHelper.cs
+++ Assets/Resources/Scripts/JsonHelper.cs
@@ -6,57 +6,112 @@ using System.IO;
 using System.Text;
 using System;
 
+/// <summary>
+/// Global 설정 저장용 함수
+/// </summary>
 public class JsonHelper : MonoBehaviour
 {
     public enum OptionType { USER_NAME, AREA_SIZE }
     private static JsonHelper Instance;
     JsonHelper() { }
     const string OPTION_FILE = "optionInfo";
+    string path;
     Option option;
+    /// <summary>
+    /// Singleton Instance 반환
+    /// </summary>
+    /// <returns>JsonHelper Instance</returns>
     public static JsonHelper GetInstance()
     {
         return Instance;
     }
-    // Start is called before the first frame update
+
     void Start()
     {
         Instance = this;
+
         option = null;
-        ReadJson();
+        //ReadJson();
+        path = new StringBuilder(Application.dataPath).Append("/Resources/").Append(OPTION_FILE).ToString();
+        //UpdateJson(OptionType.USER_NAME, "changeTest");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
+    /// <summary>
+    /// Json 파일 읽기
+    /// </summary>
     public void ReadJson()
     {
         string json = Resources.Load<TextAsset>(OPTION_FILE).ToString();
         option = JsonUtility.FromJson<Option>(json);
-        
 
+        //print($"### first : {json}");
     }
 
-    public void ReadValue()
+    /// <summary>
+    /// Json 값 읽기
+    /// </summary>
+    /// <param name="optionType">Key값에 해당하는 enum</param>
+    /// <returns>value</returns>
+    public string ReadValue(OptionType optionType)
     {
         if(option != null)
         {
-            Type type = option.userName.GetType();
-
+            switch(optionType)
+            {
+                case OptionType.USER_NAME:
+                    return option.userName;
+                    break;
+                case OptionType.AREA_SIZE:
+                    return option.areaSize.ToString();
+                    break;
+            }
 
         }
-        
+        return null;
     }
 
-    public void UpdateJson()
+    /// <summary>
+    /// Json 수정하기
+    /// </summary>
+    /// <param name="optionType">Key값에 해당하는 enum</param>
+    /// <param name="value">value</param>
+    public void UpdateJson(OptionType optionType, string value)
     {
+        if (option != null)
+        {
+            switch (optionType)
+            {
+                case OptionType.USER_NAME:
+                    option.userName = value;
+                    break;
+                case OptionType.AREA_SIZE:
+                    option.areaSize = int.Parse(value);
+                    break;
+            }
 
+        }
+        string json = JsonUtility.ToJson(option);
+        //print($"### second : {json}");
+        File.WriteAllText(path + ".json", json);
     }
 
+    /// <summary>
+    /// 설정 초기화
+    /// </summary>
     public void ResetJson()
     {
+        if(option != null)
+        {
+            option.userName = "TestName";
+            option.areaSize = 10;
 
+            string json = JsonUtility.ToJson(option);
+            File.WriteAllText(path + ".json", json);
+        }
     }
 }
