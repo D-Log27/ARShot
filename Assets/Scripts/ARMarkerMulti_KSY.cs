14d3fbac98ada0968e490c6bde7187b4de451a12
Seon <21codewar@gmail.com>
2022-04-01 18:11:42 +0900

    홀로그램이 카메라 앞에 위치하게 수정

--- Assets/Scripts/ARMarkerMulti_KSY.cs
+++ Assets/Scripts/ARMarkerMulti_KSY.cs
@@ -1,26 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
-//목표1 : AR Tracked Image Manager가 추적한 마커의 정보를 얻어오고 싶다.
-//목표2 : 만약 추적한 마커가 있다면 특정 3D 오브젝트를 마커의 위치에 배치하고 싶다.
-//목표3 : 그 3D 오브젝트가 보이게 하고 싶다.
-
 public class ARMarkerMulti_KSY : MonoBehaviour
 {
-    ARTrackedImageManager aRTrackedImageManager;
+    // 목표: ARTrackedImageManager컴포넌트에서 TrackedImage에 대한 정보를 얻어오고 싶다.
+    // 만약 Tracked된 Image가 있다면, 그 Image 위에 증강할 물체를 올려놓고 싶다.
+    // 그리고 물체가 보이게 하고 싶다.
+
+    ARTrackedImageManager arTrackedImageManager;
 
     private void Awake()
     {
-        aRTrackedImageManager = GetComponent<ARTrackedImageManager>();
+        arTrackedImageManager = GetComponent<ARTrackedImageManager>();
     }
+
+    private void OnEnable() //
+    {
+        arTrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged; //마커가 있으면 
+    }
+
+    private void OnDisable() //오브젝트가 안보인다.
+    {
+        arTrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged; //마커가 없으면
+    }
+
+    private void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        throw new NotImplementedException();
+    }
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
        
     }
-
+    
     // Update is called once per frame
     void Update()
     {
