b7b7dfeca1df63bda7ee72db140cf4ef2dda96b1
Seon <21codewar@gmail.com>
2022-04-03 14:52:22 +0900

    적 구현을 위한 플레이어 샘플 제작 - 적 구현 후 테스트를 하려면 플레이어가 필요한데 기존 플레이어가 증강이   안되서 제가 테스트용으로 사용할 플레이어를 제작했습니다. 이제 적   구현하겠습니다.

--- Assets/Scripts/Gun_KSY.cs
+++ Assets/Scripts/Gun_KSY.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// [Btn_Attack]을 터치하면 [FirePosition]에서 카메라 앞 방향으로 Ray를 쏘고, 
+// 만약 Ray가 부딪혔다면 총알자국공장에서 총알자국을 만들어 부딪힌 위치에 배치하고 싶다.
+
 public class Gun_KSY : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject bulletImpactFactory;
+    
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                GameObject efffect = Instantiate(bulletImpactFactory);
+                efffect.transform.position = hitInfo.point;
+                efffect.transform.forward = hitInfo.normal;
+                
+            }
+        }
     }
+
+
+
 }
