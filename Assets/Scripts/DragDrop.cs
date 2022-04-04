25c66ab80573dccd5b7042ac020023170991cab0
Seon <21codewar@gmail.com>
2022-04-04 22:41:39 +0900

    모바일 터치로 씬 전환 작동 테스트 테스트용으로 쓸 Start버튼 만들어서 모바일 환경에서 터치 시 InGame씬으로 넘어가는 거 확인했습니다.

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -1,33 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour
 {
+    Vector3 lastPositon;
+    Vector3 curPositon;
+    Vector3 doPosition;
+
     Collision col;
     RectTransform rTr;
     // Start is called before the first frame update
     void Start()
     {
         rTr = GetComponent<RectTransform>();
+        rTr.position = new Vector3(698, 298, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0) && EventSystem.current.currentSelectedGameObject.name == "Btn_Skill")
         {
             rTr.position = Input.mousePosition;
         }
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
-            //rTr.position = new Vector3(851, 351, 0);
-            switch (col.gameObject.name)
+            if (col == null)
             {
-                case "Skill_Player2":; break;
-                case "Skill_Player3":; break;
-                case "Skill_Player4":; break;
-                default: rTr.position = new Vector3(851, 351, 0); break;
+                rTr.position = new Vector3(698, 298, 0);
+            }
+            else
+            {
+                rTr.position = new Vector3(851, 351, 0);
+                switch (col.gameObject.name)
+                {
+                    case "Skill_Player2": print(col.gameObject.name); break;
+                    case "Skill_Player3":; break;
+                    case "Skill_Player4":; break;
+                    default: print("default"); rTr.position = new Vector3(698, 298, 0); break;
+                }
+                col = null;
+                rTr.position = new Vector3(698, 298, 0);
             }
         }
     }
@@ -36,6 +51,7 @@ public class DragDrop : MonoBehaviour
     {
 
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         col = collision;
