b790ff647e6270cd29ad8509c3257510718e6d34
SeonYoungKang <21codewar@gmail.com>
2022-04-06 16:44:16 +0900

    마커 가이드 추가

---
+++ Assets/Scripts/MarkerGuide_KSY.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerGuide_KSY : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, 5);
+    }
+}
