e11f37767886d6b6d358892c4ae5058f9d05ed4d
SeonYoungKang <21codewar@gmail.com>
2022-04-12 18:20:53 +0900

    anchor 작업 중

--- Assets/Scripts/MarkerGuide_KSY.cs
+++ Assets/Scripts/MarkerGuide_KSY.cs
@@ -7,27 +7,27 @@ using UnityEngine.UI;
 // 1.씬에서 "VIP"태그를 가진 오브젝트를 찾아서
 // 2. 만약 있으면 마커라인을 끈다.
 
-public class MarkerGuide_KSY : MonoBehaviour
+public class MarkerGuide_KSY : MonoBehaviour //
 {
     private GameObject tagVIP;
     public GameObject markerLine;
     //public GameObject tagTest; //delete!!
     //public GameObject tagTestFactory; //delete!! 
 
-    private void Start()
-    {
-        tagVIP = GameObject.FindWithTag("VIP");
-        //tagTest = GameObject.FindWithTag("VIP"); //delete!!
-    }
-
     private void Update()
     {
-        //Invoke("TagTest", 5.0f); //delete!!
+        tagVIP = GameObject.FindWithTag("VIP");
+        this.gameObject.SetActive(tagVIP == null); //조건이 true니까 켠다???
+       
 
-        if (tagVIP != null)
-        {
-            markerLine.gameObject.SetActive(false);
-        }
+        ////tagTest = GameObject.FindWithTag("VIP"); //delete!!
+ 
+        ////Invoke("TagTest", 5.0f); //delete!!
+
+        //if (tagVIP != null)
+        //{
+        //    markerLine.gameObject.SetActive(false);
+        //}
     }
 
     //void TagTest() //delete ~ 
