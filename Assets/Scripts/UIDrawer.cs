664fb81e5a030bb912faee7b8d948b6e87503b6d
SeonYoungKang <21codewar@gmail.com>
2022-04-14 13:19:11 +0900

    다시커밋

--- Assets/Scripts/UIDrawer.cs
+++ Assets/Scripts/UIDrawer.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,53 +5,26 @@ using UnityEngine;
 //UIDrawer란 평상시에는 닫혔다가 당길 때 열리는 서랍과 같은 UI
 public class UIDrawer : MonoBehaviour
 {
-    //UIDrawer 열고 닫는 버튼
-    public GameObject drawerOpener;
-    public GameObject drawerCloser;
+    public Animator uiDrawerAnimator;
 
-    //각 개체의 원위치
-    Transform drawerOpenerTrAtStart;
-    Transform drawerCloserTrAtStart;
-    Transform uiBoxTrAtStart;
-
-    //숨겨진 UI들 모음
-    public Transform uiBox;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        drawerOpenerTrAtStart = drawerOpener.transform;
-        drawerCloserTrAtStart = drawerCloser.transform;
-        uiBoxTrAtStart = uiBox;
+        uiDrawerAnimator.SetBool("DrawerOpen", false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        OpenDrawer();
-        //CloseDrawer();
-    }
-
 
     /// <summary>
     /// UI 보이기
     /// </summary>
-    private void OpenDrawer()
+    public void OnClickOpenDrawer()
     {
-        drawerOpener.transform.position = Vector3.MoveTowards(drawerOpenerTrAtStart.position, new Vector3(drawerOpenerTrAtStart.position.x, drawerOpenerTrAtStart.position.y - 10, drawerOpenerTrAtStart.position.z), 1);
-        uiBox.position = Vector3.MoveTowards(uiBoxTrAtStart.position, new Vector3(uiBoxTrAtStart.position.x, uiBoxTrAtStart.position.y - 10, uiBoxTrAtStart.position.z), 1);
-        drawerCloser.SetActive(true);
-        drawerOpener.SetActive(false);
+        uiDrawerAnimator.SetBool("DrawerOpen", true);
     }
 
     /// <summary>
     /// UI 숨기기
     /// </summary>
-    private void CloseDrawer()
+    public void OnClickCloseDrawer()
     {
-        drawerCloser.transform.position = Vector3.MoveTowards(drawerCloserTrAtStart.position, new Vector3(drawerCloserTrAtStart.position.x, drawerCloserTrAtStart.position.y - 10, drawerCloserTrAtStart.position.z), 1);
-        uiBox.position = Vector3.MoveTowards(uiBoxTrAtStart.position, new Vector3(uiBoxTrAtStart.position.x, uiBoxTrAtStart.position.y - 10, uiBoxTrAtStart.position.z), 1);
-        drawerCloser.SetActive(false);
-        drawerOpener.SetActive(true);
+        uiDrawerAnimator.SetBool("DrawerOpen", false);
     }
 }
